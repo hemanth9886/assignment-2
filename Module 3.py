1d36cc437eeec5c65911c9ab61e365f906e34805
Hemanth V D <82867837+hemanth9886@users.noreply.github.com>
2025-08-25 11:33:00 +0530

    Update and rename .py to Module 3.py

---
+++ Module 3.py
@@ -0,0 +1,18 @@
+# Assignment 2 - Module 3
+# Task 1: Check if a Number is Even or Odd
+
+# Example 1: number = 7
+num = 7
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
+
+print()  # blank line between outputs
+
+# Example 2: number = 12
+num = 12
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")

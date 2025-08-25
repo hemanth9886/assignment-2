ce493e3efb9c230df05b3caf15880a32a0500e93
Hemanth V D <82867837+hemanth9886@users.noreply.github.com>
2025-08-25 11:36:17 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,12 @@
+# Assignment 2 - Module 3
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+# Initialize sum
+total = 0
+
+# Use a for loop to iterate from 1 to 50
+for num in range(1, 51):
+    total += num   # add each number to total
+
+# Display the result (with exact format)
+print(f"The sum of numbers from 1 to 50 is : {total}")
